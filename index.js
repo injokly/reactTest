1fc39e552ad44056689ee4ce03539996214a8a33
injokly <injokly@gmail.com>
2021-03-30 21:11:11 +0900

    first Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://injo:1234@injomongodb.yrtku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex:true, useFindAndModify:false
+}).then(()=> console.log('MongoDB connected . . .'))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('hello world'))
+app.listen(port, () => console.log(`example listening port ${port}!`))
